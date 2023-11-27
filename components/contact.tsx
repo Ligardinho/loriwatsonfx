797149f37796f57a59626b174ab3ba79d5afefe2
Ligardinho <146246952+Ligardinho@users.noreply.github.com>
2023-11-27 23:20:11 +0200

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:watsonlori637@gmail.com">
-        watsonlori637@gmail.com
+        <a className="underline" href="mailto:jairampranav@gmail.com">
+        jairampranav@gmail.com
         </a>{" "}
         or through this form.
       </p>
